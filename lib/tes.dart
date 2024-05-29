c6d1daf6472af52aa6d60b83832cb584b046a51d
Wahyu Nurahmadinuh <wahyunurahmagaming@gmail.com>
2024-05-29 23:01:54 +0700

    tambah file tes.dart

---
+++ lib/tes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class tes extends StatelessWidget {
+  const tes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Judul"),
+        // backgroundColor: Color(0xff126b5a),
+      ),
+    );
+  }
+}
